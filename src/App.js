99dff0cbba433f8a1e0414d91e65ca8ea1bdd023
nexumos <nexumos@gmail.com>
2023-09-09 19:35:17 -0400

    completed exercises 6.6 & 6.7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
-import { voteFor, createAnecdote } from "./reducers/anecdoteReducer";
+import { voteFor } from "./reducers/anecdoteReducer";
+import AnecdoteForm from "./components/AnecdoteForm";
 
 const App = () => {
   const anecdotes = useSelector((state) => state);
@@ -8,14 +9,6 @@ const App = () => {
   const vote = (id) => {
     console.log("VOTE", id);
     dispatch(voteFor(id));
-  };
-
-  const create = (event) => {
-    console.log("CREATE");
-    event.preventDefault();
-    const content = event.target.newAnecdote.value;
-    event.target.newAnecdote.value = "";
-    dispatch(createAnecdote(content));
   };
 
   return (
@@ -31,12 +24,7 @@ const App = () => {
         </div>
       ))}
       <h2>create new</h2>
-      <form onSubmit={create}>
-        <div>
-          <input name="newAnecdote" />
-        </div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   );
 };
---
+++ src/components/AnecdoteForm.js
@@ -0,0 +1,25 @@
+import { useDispatch } from "react-redux";
+import { createAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch();
+
+  const create = (event) => {
+    console.log("CREATE");
+    event.preventDefault();
+    const content = event.target.newAnecdote.value;
+    event.target.newAnecdote.value = "";
+    dispatch(createAnecdote(content));
+  };
+
+  return (
+    <form onSubmit={create}>
+      <div>
+        <input name="newAnecdote" />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  );
+};
+
+export default AnecdoteForm;
