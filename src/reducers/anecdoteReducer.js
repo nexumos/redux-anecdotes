a3a5d16fdd358b0b0359ec34b033495392f4d56e
nexumos <nexumos@gmail.com>
2023-09-10 09:22:02 -0400

    completed exercises 6.10 & 6.11

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const anecdotesAtStart = [
   "If it hurts, do it more often",
   "Adding manpower to a late software project makes it later!",
@@ -17,44 +19,75 @@ const asObject = (anecdote) => {
   };
 };
 
-export const voteFor = (id) => {
-  return {
-    type: "VOTE",
-    payload: { id },
-  };
-};
+// export const voteFor = (id) => {
+//   return {
+//     type: "VOTE",
+//     payload: { id },
+//   };
+// };
 
-export const createAnecdote = (anecdote) => {
-  return {
-    type: "CREATE",
-    payload: {
-      content: anecdote,
-      id: getId(),
-      votes: 0,
-    },
-  };
-};
+// export const createAnecdote = (anecdote) => {
+//   return {
+//     type: "CREATE",
+//     payload: {
+//       content: anecdote,
+//       id: getId(),
+//       votes: 0,
+//     },
+//   };
+// };
 
 const initialState = anecdotesAtStart
   .map(asObject)
   .toSorted((a, b) => b.votes - a.votes);
 
-const anecdoteReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "VOTE":
-      const anecdoteToVoteFor = state.find((n) => n.id === action.payload.id);
+// const anecdoteReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "VOTE":
+//       const anecdoteToVoteFor = state.find((n) => n.id === action.payload.id);
+//       const changedAnecdote = {
+//         ...anecdoteToVoteFor,
+//         votes: anecdoteToVoteFor.votes + 1,
+//       };
+//       return state
+//         .map((n) => (n.id !== action.payload.id ? n : changedAnecdote))
+//         .toSorted((a, b) => b.votes - a.votes);
+//     case "CREATE":
+//       return state.concat(action.payload).toSorted((a, b) => b.votes - a.votes);
+//     default:
+//       return state.toSorted((a, b) => b.votes - a.votes);
+//   }
+// };
+
+//export default anecdoteReducer;
+
+const anecdoteSlice = createSlice({
+  name: "anecdotes",
+  initialState,
+  reducers: {
+    createAnecdote(state, action) {
+      const content = action.payload;
+      state
+        .push({
+          content,
+          votes: 0,
+          id: getId(),
+        })
+        .sort((a, b) => b.votes - a.votes);
+    },
+    voteFor(state, action) {
+      const id = action.payload;
+      const anecdoteToChange = state.find((n) => n.id === id);
       const changedAnecdote = {
-        ...anecdoteToVoteFor,
-        votes: anecdoteToVoteFor.votes + 1,
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1,
       };
       return state
-        .map((n) => (n.id !== action.payload.id ? n : changedAnecdote))
-        .toSorted((a, b) => b.votes - a.votes);
-    case "CREATE":
-      return state.concat(action.payload).toSorted((a, b) => b.votes - a.votes);
-    default:
-      return state.toSorted((a, b) => b.votes - a.votes);
-  }
-};
+        .map((note) => (note.id !== id ? note : changedAnecdote))
+        .sort((a, b) => b.votes - a.votes);
+    },
+  },
+});
 
-export default anecdoteReducer;
+export const { createAnecdote, voteFor } = anecdoteSlice.actions;
+export default anecdoteSlice.reducer;
